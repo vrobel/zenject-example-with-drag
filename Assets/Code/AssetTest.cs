7c6a15849a293c25240fd2138f48dbb835d4deff
Michał Wróblewski <michal.wroblewski@setapp.pl>
2024-03-03 18:50:07 +0100

    Switched to IInitializePotentialDragHandler in Library panel

---
+++ Assets/Code/AssetTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Code
+{
+    public class AssetTest : MonoBehaviour, IInitializePotentialDragHandler, IDragHandler
+    {
+        [SerializeField] private Transform draggedObject;
+        public void OnInitializePotentialDrag(PointerEventData eventData)
+        {
+            Debug.Assert(draggedObject, this);
+            
+            eventData.pointerDrag = eventData.selectedObject =
+                draggedObject.gameObject;
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            //not used. only to trigger actual begin of Drag sequence.
+        }
+    }
+}
